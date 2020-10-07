e920b7748cca7b45bf2359180dfb746fd7ae3eeb
Peter <36786686+petardmnv@users.noreply.github.com>
2020-10-07 19:11:00 +0300

    Delete bracketsprinter.py

--- bracketsprinter.py
+++
@@ -1,8 +0,0 @@
-text = input()
-brackets = []
-for i in range(len(text)):
-    if text[i] == '(':
-        brackets.append(i)
-    elif text[i] == ')':
-        index = brackets.pop();
-        print(text[index: i + 1])
