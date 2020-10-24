04df515b103df2a98513d4bfa4d788df76dc74d2
Peter <36786686+petardmnv@users.noreply.github.com>
2020-10-24 13:47:58 +0300

    Add Exam practise and Final Exam exercises 300/300

--- Exam Preparation/Checkmate.py
+++ Exam Preparation/Checkmate.py
@@ -5,13 +5,14 @@ for i in range(8):
     my_l = input().split(" ")
     for j in range(8):
         matrix[i][j] = my_l[j]
-
 k_r = 0
 k_c = 0
 for i in range(8):
     if matrix[i].__contains__("K"):
         k_c = matrix[i].index("K")
         k_r = i
+
+
 def find_queen(matrix):
     res = []
     for i in range(8):
@@ -20,25 +21,11 @@ def find_queen(matrix):
             res.append(matrix[i].index("Q"))
             return res
     return [-1, -1]
-#(x+1, y): one step horizontal move to the right.
-#(x-1, y): one step horizontal move to the left.
-#(x+1, y+1): one step diagonal move up-right.
-#(x-1, y-1): one step diagonal move down-left.
-#(x-1, y+1): one step diagonal move left-up.
-#(x+1, y-1): one step diagonal move right-down.
-#(x, y+1): one step downward.
-#(x, y-1): one step upward.
+
 
 def q_moves(matrix, x, y):
-    moves = []
-    moves.append(["x+i", "y"])
-    moves.append(["x-i", "y"])
-    moves.append(["x+i", "y+i"])
-    moves.append(["x-i", "y-i"])
-    moves.append(["x-i", "y+i"])
-    moves.append(["x+i", "y-i"])
-    moves.append(["x", "y+i"])
-    moves.append(["x", "y-i"])
+    moves = [["x+i", "y"], ["x-i", "y"], ["x+i", "y+i"], ["x-i", "y-i"], ["x-i", "y+i"], ["x+i", "y-i"], ["x", "y+i"],
+             ["x", "y-i"]]
     for m in moves:
         for i in range(1, 8):
             r = eval(m[0])
@@ -63,7 +50,3 @@ if len(result) == 0:
     print("The king is safe!")
 else:
     [print(re) for re in result]
-
-
-
-
